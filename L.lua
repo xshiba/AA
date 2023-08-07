7574c4b10dd4aff97687c35e81079a32c05bf66a
xshiba <92438125+xshiba@users.noreply.github.com>
2023-08-07 23:15:10 +0700

    Update L.lua

--- L.lua
+++ L.lua
@@ -1,22 +1,23 @@
-task.spawn(function()
-    local gg = getrawmetatable(game)
-    local old = gg.__namecall
-    setreadonly(gg,false)
-    gg.__namecall = newcclosure(function(...)
-        local method = getnamecallmethod()
-        local args = {...}
-        if tostring(method) == "InvokeServer" then
-            if tostring(args[1]) == "SkillAction" then
-                if (args[3]) then
-                    if args[3]["MouseHit"] and (SaveSettings["Main"]["AutoFarm"] or SaveSettings["Main"]["AutoSecondSea"] or SaveSettings["Main"]["AutoLPQuests"] or SaveSettings["Main"]["AutoSK"] or SaveSettings["Main"]["AutoGhostShip"] or SaveSettings["Main"]["AutoHydra"] or SaveSettings["Main"]["AutoFarmBosses"]) and MobP then
-                        pcall(function()
-                            args[3]["MouseHit"] = MobP
-                            return old(unpack(args))
-                        end)
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt,false)
+mt.__namecall = function(...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if method == "InvokeServer" then
+        if tostring(args[1]) == "SkillAction" then
+            if _G.ActiveSkill then
+                if MobP ~= nil then
+                    if not args[3] then
+                        return old(...)
+                    end
+                    if args[3].Type == "Up" or args[3].Type == "Down" then
+                        args[3].MouseHit = MobP
+                        return old(unpack(args))
                     end
                 end
             end
         end
-        return old(...)
-    end)
-end)
+    end
+    return old(...)
+end
